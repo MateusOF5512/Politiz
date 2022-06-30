738dd7b34c5f3b89041d012d380402516b6a14b0
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2022-06-30 11:44:17 -0300

    Add files via upload

---
+++ Stream_variables.py
@@ -0,0 +1,98 @@
+path_data = "vestibular.xlsx"
+
+# ---------------------------------------------------------------------------------------
+
+# ------------------------------------------- TITULO
+html_title="""
+<h1 style="font-size: 50px; color:#003893; font-family:sans-serif">Instâncias Representativas
+ <h2 style="font-size: 34px;color:#003893;font-family:sans-serif">Eng. da Produção - UFSC
+ <hr style= "display: block;
+  margin-top: 0.2em;
+  margin-bottom: 0.2em;
+  margin-left: auto;
+  margin-right: auto;
+  border-style: inset;
+  border-width: 1.5px;">
+<p style="color:Gainsboro; text-align: right;">Última atualização dos dados: 01 de julho de 2022 </p>
+"""
+#------------------------------------------ RODAPE
+html_rodape="""
+<hr style= "  display: block;
+  margin-top: 0.2px;
+  margin-bottom: 0.2px;
+  margin-left: auto;
+  margin-right: auto;
+  border-style: inset;
+  border-width: 1.5px;">
+<p style="color:Gainsboro; text-align: right;">By: mateus7ortiz@gmail.com</p>
+"""
+# ---------------------------------------- ESTRUTURA
+html_card_header_0A_1_11="""
+<div class="card">
+  <div class="card-body" style="border-radius: 10px 10px 0px 0px; background: #4169E1; padding-top: 5px; width: 1200px; height: 50px;">
+    <h5 class="card-title" style="font-size: 24px; background-color:#4169E1; color:#F5F5F5; font-family:sans-serif; text-align: center; padding: 5px 5px;"
+    >Descrição dos Dados</h5>
+  </div>
+</div>
+"""
+html_card_body_0A_1_11="""
+<div class="card" style="border-radius: 0px 0px 10px 10px; background: #F5F5F5; width: 1200px; height: 150px;
+font-size: 16px;  padding-top: 15px; padding-left: 15px; padding-right:15px">
+    <h8 class="card-title" style="background-color:#F5F5F5; color:#0d0d0d; 
+        font-family:sans-serif; text-align: justify;"
+        >As visualizações apresentam os Dados referentes à Campanha de Vacinação contra Covid-19, 
+        da população residente em Florianópolis/SC, para melhor contraste das informações, também utilizamos os
+        Dados do CENSO de 2010 (mais recente).</h8>
+</div>
+"""
+
+# ---------------------------
+html_header_10="""
+<div class="card">
+  <div class="card-body">
+    <h2 class="card-title" style="color:#003893; font-family:sans-serif; text-align: center; padding: 10px 0;"
+    >Números sobre o Ensino</h2>
+  </div>
+</div>
+"""
+
+html_subheader_11_A0="""
+<div class="card">
+  <div class="card-body" style="border-radius: 10px 10px 10px 10px; background: #003893; padding-top: 5px; width: 1200px; height: 60px;">
+    <h3 class="card-title" style="background-color:#003893; color:#F5F5F5; font-family:sans-serif; text-align: center; padding: 10px 10px;"
+    >Vestibular, entra e saída de estudantes</h3>
+  </div>
+</div>
+"""
+
+html_card_header_11_A1="""
+<div class="card">
+  <div class="card-body" style="border-radius: 10px 10px 0px 0px; background: #003893; padding-top: 5px; width: 550 px; height: 40px;">
+    <h5 class="card-title" style="background-color:#003893; color:#F5F5F5; font-family:sans-serif; text-align: center; padding: 5px 5;"
+    >Candidatos Incritos e Vagas Oferecidas</h5>
+  </div>
+</div>
+"""
+html_card_header_11_A2="""
+<div class="card">
+  <div class="card-body" style="border-radius: 10px 10px 0px 0px; background: #003893; padding-top: 5px; width: 550px; height: 40px;">
+    <h5 class="card-title" style="background-color:#003893; color:#F5F5F5; font-family:sans-serif; text-align: center; padding: 5px 5;"
+    >Tabela com Valores</h5>
+  </div>
+</div>
+"""
+html_card_header_11_A3="""
+<div class="card">
+  <div class="card-body" style="border-radius: 10px 10px 0px 0px; background: #4169E1; padding-top: 5px; width: 360px; height: 40px;">
+    <h5 class="card-title" style="background-color:#4169E1; color:#F5F5F5; font-family:sans-serif; text-align: center; padding: 5px 5;"
+    >Vagas Oferecidas</h5>
+  </div>
+</div>
+"""
+
+
+
+
+
+
+
